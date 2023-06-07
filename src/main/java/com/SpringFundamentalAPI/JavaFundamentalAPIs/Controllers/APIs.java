c3a40e3a9dd2f188cc313ecea8d3e6e774bfaef8
James M W <jimmymugo1@gmail.com>
2023-06-07 17:13:12 +0300

    Add Json Variable

--- src/main/java/com/SpringFundamentalAPI/JavaFundamentalAPIs/Controllers/APIs.java
+++ src/main/java/com/SpringFundamentalAPI/JavaFundamentalAPIs/Controllers/APIs.java
@@ -20,7 +20,4 @@ public class APIs {
         return null;
     }
 
-
-
-
 }
--- src/main/java/com/SpringFundamentalAPI/JavaFundamentalAPIs/Util/Helpers.java
+++ src/main/java/com/SpringFundamentalAPI/JavaFundamentalAPIs/Util/Helpers.java
@@ -10,16 +10,28 @@ public class Helpers {
 
     /*Using Object Mapper Example*/
 
+
+    /**Convert JSON to Object Array*/
     public static Object mapper() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         String user = "{\"id\":1,\"name\":\"RehamMuzzamil\",\"qualification\":\"MBBS\",\"yearsOfExperience\":1.5}";
 
+        /**
+         * Use ->readValue<- to convert a JSON String into a Java Object
+         * */
         Users userData = objectMapper.readValue(user, Users.class);
 
         return userData;
 
     }
+
+    /**
+     * Convert Json String to Java Object with Readers
+     * The readerValue() methods accepts a Reader
+     * **/
+//    public static Object
+
 
 
 }
